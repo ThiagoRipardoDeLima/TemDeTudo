562d074379de2ed20b1a5e207e97ef5417331257
Thiago <thiago.ripardo.86@gmail.com>
2018-05-25 22:57:41 -0400

    Exercicio Proprosto 2 - add classes de marca e carro

---
+++ OOP/RevendaDeCarro/model/Carro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RevendaDeCarro.model
+{
+    class Carro
+    {
+        public int codigo { get; private set; }
+        public string modelo { get; private set; }
+        public int ano { get; private set; }
+        public double precoBasico { get; private set; }
+
+        public double precoTotal() { return 0.00; }
+    }
+}
